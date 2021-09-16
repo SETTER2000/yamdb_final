ba29cb58e7f9339efbab535db085e25e7f5b351a
SETTER <info@lphp.ru>
2021-09-16 16:43:02 +0300

    All tests

---
+++ api/tests.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+
+from django.test import Client, TestCase
+
+
+class StaticPagesURLTests(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_about_url_exists_at_desired_location(self):
+        """Проверка доступности страниц."""
+        response = self.guest_client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        response = self.guest_client.get('/titles/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    # def test_page_shows_correct_context(self):
+    #     """Проверка контекста страниц."""
+    #     response = self.guest_client.get('/')
+    #     self.assertContains(response, 'У меня получилось!')
+    #
+    #     response = self.guest_client.get('/second_page/')
+    #     self.assertContains(response, 'А это вторая страница!')
